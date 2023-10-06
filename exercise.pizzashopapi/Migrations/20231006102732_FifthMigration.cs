6bb6c9a1e751993190d44916ba68ce526b930a94
Max Kroon <maxkroon.18@gmail.com>
2023-10-06 14:28:47 +0200

    Core

---
+++ exercise.pizzashopapi/Migrations/20231006102732_FifthMigration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exercise.pizzashopapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FifthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PizzaId",
+                table: "Orders",
+                column: "PizzaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Pizzas_PizzaId",
+                table: "Orders",
+                column: "PizzaId",
+                principalTable: "Pizzas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Pizzas_PizzaId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PizzaId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderDate",
+                table: "Orders");
+        }
+    }
+}
--- exercise.pizzashopapi/Repository/Repository.cs
+++ exercise.pizzashopapi/Repository/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using exercise.pizzashopapi.Data;
 using exercise.pizzashopapi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace exercise.pizzashopapi.Repository
 {
@@ -7,7 +8,23 @@ namespace exercise.pizzashopapi.Repository
     {
         public IEnumerable<Order> GetOrders()
         {
-            throw new NotImplementedException();
+            using (var db = new DataContext())
+            {
+                return db.Orders.Include(o => o.Customer).Include(o => o.Pizza).ToList();
+              /*  var orders = from order in db.Orders
+                    join pizza in db.Pizzas on order.PizzaId equals pizza.Id
+                    join customer in db.Customers on order.CustomerId equals customer.Id
+                    select new Order
+                    {
+                    Id = order.Id,
+                    Pizza = pizza,
+                    Customer = customer,
+                                              
+                    };
+
+                return orders.ToList();*/
+            }
         }
     }
 }
+
