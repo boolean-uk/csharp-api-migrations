dde23f79c6cee30de9c6657042ced32851dfefa2
spiroskafiris <spiroskafiris@gmail.com>
2023-10-07 22:17:01 +0300

    "Spiros Kafiris"

--- exercise.pizzashopapi/Data/Seeder.cs
+++ exercise.pizzashopapi/Data/Seeder.cs
@@ -10,23 +10,28 @@ namespace exercise.pizzashopapi.Data
             {
                 if(!db.Customers.Any())
                 {
-                    db.Add(new Customer() { Name="Nigel" });
-                    db.Add(new Customer() { Name = "Dave" });
+                    db.Add(new Customer() { Name="Nigel", Address="LondonAve 116" });
+                    db.Add(new Customer() { Name = "Dave", Address = "LondonAve 116" });
+                    db.Add(new Customer() { Name = "Spiros", Address = "Ermou 150" });
                     db.SaveChanges();
                 }
                 if(!db.Pizzas.Any())
                 {
-                    db.Add(new Pizza() { Name = "Cheese & Pineapple" });
-                    db.Add(new Pizza() { Name = "Vegan Cheese Tastic" });
+                    db.Add(new Pizza() { Name = "Cheese & Pineapple", Price = 10 });
+                    db.Add(new Pizza() { Name = "Vegan Cheese Tastic", Price = 10 });
+                    db.Add(new Pizza() { Name = "Barbeque Hot Chilli Sauce", Price = 10 });
                     db.SaveChanges();
 
                 }
 
                 //order data
-                if(1==1)
+                if (!db.Orders.Any())
                 {
-
+                    db.Add(new Order() { OrderDate = "6/10/23", Price = 10, CustomerId = 1, PizzaId = 1 });
+                    db.Add(new Order() { OrderDate = "6/10/23", Price = 10, CustomerId = 2, PizzaId = 2 });
+                    db.Add(new Order() { OrderDate = "6/10/23", Price = 10, CustomerId = 3, PizzaId = 3 });
                     db.SaveChanges();
+
                 }
             }
         }
---
+++ exercise.pizzashopapi/Migrations/20231007185636_OrderForeignKeysSecond.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace exercise.pizzashopapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderForeignKeysSecond : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderDate = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    CustomerId = table.Column<int>(type: "integer", nullable: false),
+                    PizzaId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Pizzas_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PizzaId",
+                table: "Orders",
+                column: "PizzaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
