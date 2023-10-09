fb39415f4edf35656a641296d243a6dce332fbc3
konas <konas@LAPTOP-1GHMR8NV>
2023-10-09 12:55:54 +0300

    updated Seeder.cs

--- exercise.pizzashopapi/Data/Seeder.cs
+++ exercise.pizzashopapi/Data/Seeder.cs
@@ -4,28 +4,55 @@ namespace exercise.pizzashopapi.Data
 {
     public static class Seeder
     {
+        private static List<Customer> _customers = new List<Customer>()
+        {
+            new Customer() { Name = "Dave"},
+            new Customer() { Name = "Nigel"}
+        };
+
+        private static List<Pizza> _pizzas = new List<Pizza>()
+        {
+            new Pizza() { Name = "Cheese & Pineapple" },
+            new Pizza() { Name = "Vegan Cheese Tastic" }
+        };
+
+        private static List<Order> _orders = new List<Order>()
+        {
+            new Order() {
+                OrderDate = DateTime.Now,
+                CustomerId = _customers[0].Id,
+                Customer = _customers[0],
+                PizzaId = _pizzas[0].Id,
+                Pizza = _pizzas[0]
+            },
+            new Order() {
+                OrderDate = DateTime.Now,
+                CustomerId = _customers[1].Id,
+                Customer = _customers[1],
+                PizzaId = _pizzas[1].Id,
+                Pizza = _pizzas[1]
+            }
+        };
+
         public static void SeedPizzaShopApi(this WebApplication app)
         {
             using(var db = new DataContext())
             {
                 if(!db.Customers.Any())
                 {
-                    db.Add(new Customer() { Name="Nigel" });
-                    db.Add(new Customer() { Name = "Dave" });
+                    db.AddRange(_customers);
                     db.SaveChanges();
                 }
                 if(!db.Pizzas.Any())
                 {
-                    db.Add(new Pizza() { Name = "Cheese & Pineapple" });
-                    db.Add(new Pizza() { Name = "Vegan Cheese Tastic" });
+                    db.AddRange(_pizzas);
                     db.SaveChanges();
-
                 }
 
                 //order data
-                if(1==1)
+                if(!db.Orders.Any())
                 {
-
+                    db.AddRange(_orders);
                     db.SaveChanges();
                 }
             }
