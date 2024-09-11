0aa66d318053f59cbacd8bc4b6ee725ad6619e3f
Jonas <jonashalvorsen2013@live.no>
2024-09-11 14:31:10 +0200

    repository-setup

--- exercise.pizzashopapi/Repository/IRepository.cs
+++ exercise.pizzashopapi/Repository/IRepository.cs
@@ -4,8 +4,24 @@ namespace exercise.pizzashopapi.Repository
 {
     public interface IRepository
     {
-        IEnumerable<Order> GetOrdersByCustomer(int id);
-        
+        // ------ Orders ------
+        Task<IEnumerable<Order>> GetOrders();
+        Task<IEnumerable<Order>> GetOrdersByCustomer(int id);
+        Task<Order> GetOrderById(int customerId, int pizzaId);
+        Task<Order> CreateOrder(Order entity);
+        Task<Order> DeleteOrder(int customerId, int pizzaI);
+
+        // ------ Pizza ------
+        Task<IEnumerable<Pizza>> GetPizzas();
+        Task<Pizza> GetPizzaById(int id);
+        Task<Pizza> CreatePizza(Pizza entity);
+        Task<Pizza> DeletePizza(int id);
+
+        // ------ Pizza ------
+        Task<IEnumerable<Customer>> GetCustomers();
+        Task<Customer> GetCustomerById(int id);
+        Task<Customer> CreateCustomer(Customer entity);
+        Task<Customer> DeleteCustomer(int id);
 
     }
 }
--- exercise.pizzashopapi/Repository/Repository.cs
+++ exercise.pizzashopapi/Repository/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using exercise.pizzashopapi.Data;
 using exercise.pizzashopapi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace exercise.pizzashopapi.Repository
 {
@@ -12,9 +13,110 @@ namespace exercise.pizzashopapi.Repository
             _db = db;
         }
 
-        public IEnumerable<Order> GetOrdersByCustomer(int id)
+        // ------ Orders ------
+        public async Task<IEnumerable<Order>> GetOrders()
         {
-            throw new NotImplementedException();
+            return await _db.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Pizza)
+                .ToListAsync();
+        }
+        public async Task<IEnumerable<Order>> GetOrdersByCustomer(int id)
+        {
+            return await _db.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Pizza)
+                .Where(o => o.CustomerId == id)
+                .ToListAsync();
+        }
+        public async Task<Order> GetOrderById(int customerId, int pizzaId)
+        {
+            return await _db.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Pizza)
+                .FirstOrDefaultAsync(o => o.CustomerId == customerId && 
+                                          o.PizzaId == pizzaId);
+        }
+        public async Task<Order> CreateOrder(Order entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<Order> DeleteOrder(int customerId, int pizzaId)
+        {
+            var target = await _db.Orders.FirstOrDefaultAsync(o => o.CustomerId == customerId && o.PizzaId == pizzaId);
+            if (target == null)
+            {
+                return null;
+            }
+            _db.Orders.Remove(target);
+            await _db.SaveChangesAsync();
+            return target;
+        }
+
+
+        //  ------ Customers ------
+
+        public async Task<IEnumerable<Customer>> GetCustomers()
+        {
+            return await _db.Customers
+                .Include(c => c.Order)
+                .ToListAsync();
+        }
+        public async Task<Customer> GetCustomerById(int id)
+        {
+            return await _db.Customers
+                .Include(c => c.Order)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task<Customer> CreateCustomer(Customer entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<Customer> DeleteCustomer(int id)
+        {
+            var target = await _db.Customers.FirstOrDefaultAsync(c => c.Id == id);
+            if (target == null)
+            {
+                return null;
+            }
+            _db.Customers.Remove(target);
+            await _db.SaveChangesAsync();
+            return target;
+        }
+
+        // ------ Pizzas ------
+        public async Task<IEnumerable<Pizza>> GetPizzas()
+        {
+            return await _db.Pizzas
+                .Include(p => p.Order)
+                .ToListAsync();
+        }
+        public async Task<Pizza> GetPizzaById(int id)
+        {
+            return await _db.Pizzas
+                .Include(p => p.Order)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task<Pizza> CreatePizza(Pizza entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<Pizza> DeletePizza(int id)
+        {
+            var target = await _db.Pizzas.FirstOrDefaultAsync(p => p.Id == id);
+            if (target == null)
+            {
+                return null;
+            }
+            _db.Pizzas.Remove(target);
+            await _db.SaveChangesAsync();
+            return target;
         }
     }
 }
