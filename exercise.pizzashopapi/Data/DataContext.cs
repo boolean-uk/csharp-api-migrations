cb93cdbcc70c6b204fc00c301d21406e932b531e
Julia <julia.r.lindgren@gmail.com>
2024-09-12 14:17:29 +0200

    Core complete

--- exercise.pizzashopapi/Data/DataContext.cs
+++ exercise.pizzashopapi/Data/DataContext.cs
@@ -27,17 +27,20 @@ namespace exercise.pizzashopapi.Data
         {
             modelBuilder.Entity<Customer>().HasData(
                new Customer { Id = 1, Name = "Nigel" },
-               new Customer { Id = 2, Name = "Dave" }
+               new Customer { Id = 2, Name = "Dave" },
+               new Customer { Id = 3, Name = "Julia" }
             );
 
             modelBuilder.Entity<Pizza>().HasData(
                 new Pizza { Id = 1, Name = "Cheese & Pineapple", Price = 10 },
-                new Pizza { Id = 2, Name = "Vegan Cheese Tastic", Price = 13 }
+                new Pizza { Id = 2, Name = "Vegan Cheese Tastic", Price = 13 },
+                new Pizza { Id = 3, Name = "Pepperoni", Price = 15 }
             );
 
             modelBuilder.Entity<Order>().HasData(
                 new Order { Id = 1, CustomerId = 1, PizzaId = 2 },
-                new Order { Id = 2, CustomerId = 2, PizzaId = 1 }
+                new Order { Id = 2, CustomerId = 2, PizzaId = 1 },
+                new Order { Id = 3, CustomerId = 3, PizzaId = 3 }
             );
 
             base.OnModelCreating(modelBuilder);
---
+++ exercise.pizzashopapi/Migrations/20240912121707_UpdateSeeder.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exercise.pizzashopapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "customers",
+                columns: new[] { "id", "name" },
+                values: new object[] { 3, "Julia" });
+
+            migrationBuilder.InsertData(
+                table: "pizzas",
+                columns: new[] { "id", "name", "price" },
+                values: new object[] { 3, "Pepperoni", 15m });
+
+            migrationBuilder.InsertData(
+                table: "order",
+                columns: new[] { "id", "customer_id", "pizza_id" },
+                values: new object[] { 3, 3, 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "order",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "customers",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "pizzas",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
